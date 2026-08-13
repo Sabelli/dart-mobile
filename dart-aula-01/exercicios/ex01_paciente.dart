2c891683b2d0855fcbe4feaa779c104a0c96ed74
Sabelli <sabellinho@gmail.com>
2026-08-12 21:02:28 -0300

    feat: adiciona exercícios aula 12/08

---
+++ dart-aula-01/exercicios/ex01_paciente.dart
@@ -0,0 +1,11 @@
+void main(){
+  final id = 1;
+  String nome = 'Ana Silva';
+  String cpf = '123.456.789-00';
+  String email = 'ana@email.com';
+  String telefone = '(11) 99999-8888';
+
+  print('Paciente #$id: $nome');
+  print('CPF: $cpf E-mail: $email');
+  print('Contato: $email | $telefone');
+}
